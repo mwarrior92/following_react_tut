425b5675bf4b31970a0df511dd4b6f2945f52eee
mwarrior92 <warrior@u.northwestern.edu>
2019-01-09 17:01:07 -0600

    Click makes X or O

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*
 class Square extends React.Component {
   render() {
     return (
@@ -11,6 +12,14 @@ class Square extends React.Component {
       </button>
     );
   }
+}*/
+
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
@@ -18,12 +27,16 @@ class Board extends React.Component {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
 	handleClick(i){
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({squares: squares});
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
 	}
   renderSquare(i) {
     return (
@@ -35,7 +48,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+(this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
